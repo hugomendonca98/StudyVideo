0c9a12601ec9e9038919fd922d203a62c0667796
hugomendonca98 <hugomendonca9@gmail.com>
2022-03-29 18:10:34 -0300

    header banner

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,43 @@
 import LinkButton from '@/components/LinkButton';
 import NavBar from '@/components/NavBar';
-import { Item, MenuItems } from './styles';
+import {
+  Header,
+  HeaderText,
+  HeaderTitle,
+  Item,
+  MenuItems,
+  Container,
+} from './styles';
 
 export default function Home(): JSX.Element {
   return (
-    <NavBar>
-      <MenuItems>
-        <Item>
-          <LinkButton
-            linkTo="#a"
-            background="#fff"
-            textColor="#6e2fbd"
-            border="1px solid #E0E0E0"
-          >
-            Entrar
-          </LinkButton>
-        </Item>
-        <Item>
-          <LinkButton linkTo="#a">Criar Conta</LinkButton>
-        </Item>
-      </MenuItems>
-    </NavBar>
+    <>
+      <NavBar>
+        <MenuItems>
+          <Item>
+            <LinkButton
+              linkTo="#a"
+              background="#fff"
+              textColor="#6e2fbd"
+              border="1px solid #E0E0E0"
+            >
+              Entrar
+            </LinkButton>
+          </Item>
+          <Item>
+            <LinkButton linkTo="#a">Criar Conta</LinkButton>
+          </Item>
+        </MenuItems>
+      </NavBar>
+      <Header>
+        <Container>
+          <HeaderText>
+            <HeaderTitle>Estude onde quiser.</HeaderTitle>
+            <HeaderTitle>Forme grupos.</HeaderTitle>
+            <HeaderTitle>Totalmente online.</HeaderTitle>
+          </HeaderText>
+        </Container>
+      </Header>
+    </>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const teste = styled.div``;
+import banner from '@/assets/banner.png';
 
 export const MenuItems = styled.ul`
   display: flex;
@@ -11,4 +11,34 @@ export const Item = styled.li`
   list-style: none;
   font-family: Roboto;
   margin-right: 10px;
+`;
+
+export const Header = styled.header`
+  background: url(${banner});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 400px;
+  background-color: #6e2fbd;
+  color: #fff;
+  font-size: 30px;
+`;
+
+export const HeaderText = styled.div`
+  padding-top: 220px;
+`;
+
+export const HeaderTitle = styled.div`
+  background: #2d2d2d;
+  width: 400px;
+  margin: 10px;
+  padding: 5px;
+  font-family: Roboto, sans-serif;
+  font-size: 40px;
+  font-weight: 500;
+`;
+
+export const Container = styled.div`
+  width: 1140px;
+  margin: auto;
 `;
